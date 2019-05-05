0f814ad2532fe620cef86c6a7cc4c97dae4f1380
Jhon Marin <yoned1996@gmail.com>
2019-05-04 21:30:20 -0500

    Fauna-2

--- js/js.js
+++ js/js.js
@@ -2,12 +2,21 @@
 const $start = document.getElementById('start');
 const $principal = document.getElementById('principal');
 const $nav = document.getElementById('nav');
+const $close = document.getElementById('close');
 
 $start.addEventListener('click',iniciar);
+$close.addEventListener('click',cerrar)
 
 function iniciar(){
     $principal.style.animation='fadeOut 2s forwards';
     $principal.style.display='none' ;
     $nav.style.display='block' ;
     $nav.style.animation='fadeIn 2s forwards';
+}
+
+function cerrar(){
+    $principal.style.animation='fadeIn 2s forwards';
+    $principal.style.display='block' ;
+    $nav.style.display='none' ;
+    $nav.style.animation='fadeOut 2s forwards';
 }
